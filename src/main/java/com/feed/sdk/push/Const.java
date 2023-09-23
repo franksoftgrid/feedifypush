0499ea9419c0fc92c915578c8d89e86f239a1c86
Abdul Aziz Ansari <abduldblogger@gmail.com>
2023-09-23 14:44:25 +0530

    API changed to https://app.feedify.net/push/register

--- src/main/java/com/feed/sdk/push/Const.java
+++ src/main/java/com/feed/sdk/push/Const.java
@@ -4,7 +4,7 @@ package com.feed.sdk.push;
  * This class provided constants for library, like preferences key and API endpoints
  */
 public class Const {
-    public static final String PUSH_REGISTER = "https://feedify.net/push/register";
+    public static final String PUSH_REGISTER = "https://app.feedify.net/push/register";
     public static final String PREF_ENABLE_KEY = "PREF_ENABLE_KEY";
     public static final String FETCH_AD = "https://deliver.feedify.net/fetch-ad";
     public static final String DELIVERY_COUNT_API = "https://deliver.feedify.net/deliveryCount";
